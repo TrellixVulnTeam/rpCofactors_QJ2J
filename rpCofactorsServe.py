91c3bad634a731fc9f655fbfe13cfe829866aa35
mdulac <melchior.dulac@inra.fr>
2019-10-29 11:54:19 +0100

    to test

---
+++ rpCofactorsServe.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+"""
+Created on September 21 2019
+
+@author: Melchior du Lac
+@description: Backend rpCofactors server
+
+"""
+
+import os
+import shutil
+import json
+import libsbml
+from datetime import datetime
+from flask import Flask, request, jsonify, send_file, abort
+from flask_restful import Resource, Api
+import io
+import tarfile
+import csv
+import sys
+import glob
+
+sys.path.insert(0, '/home/')
+import rpCofactors
+import rpSBML
+
+#######################################################
+############## REST ###################################
+#######################################################
+
+app = Flask(__name__)
+api = Api(app)
+
+rpcache = rpCache.rpCache()
+
+
+## Stamp of rpCofactors
+#
+#
+def stamp(data, status=1):
+    appinfo = {'app': 'rpCofactors', 'version': '1.0', 
+               'author': 'Melchior du Lac',
+               'organization': 'BRS',
+               'time': datetime.now().isoformat(), 
+               'status': status}
+    out = appinfo.copy()
+    out['data'] = data
+    return out
+
+
+## REST App.
+#
+#
+class RestApp(Resource):
+    def post(self):
+        return jsonify(stamp(None))
+    def get(self):
+        return jsonify(stamp(None))
+
+
+## Run a single 
+#
+#
+def runSingleSBML(rpcofactors, member_name, rpsbml_string, path_id, compartment_id):
+    #open one of the rp SBML files
+    rpsbml = rpSBML.rpSBML(member_name, libsbml.readSBMLFromString(rpsbml_string))
+    if rpcofactors.addCofactors(rpsbml, compartment_id, path_id):
+        return libsbml.writeSBMLToString(rpsbml.document).encode('utf-8')
+    else:
+        return ''
+
+
+##
+#
+#
+def runCofactors_mem(rpcofactors, inputTar, outputTar, path_id='rp_pathway', compartment_id='MNXC3'):
+    #loop through all of them and run FBA on them
+    with tarfile.open(outputTar, 'w:xz') as tf:
+        with tarfile.open(inputTar, 'r:xz') as in_tf:
+            for member in in_tf.getmembers():
+                if not member.name=='':
+                    data = singleCofactors(rpcofactors,
+                            member.name,
+                            in_tf.extractfile(member).read().decode('utf-8'),
+                            path_id,
+                            compartment_id)
+                    if not data=='':
+                        fiOut = io.BytesIO(data)
+                        info = tarfile.TarInfo(member.name)
+                        info.size = len(data)
+                        tf.addfile(tarinfo=info, fileobj=fiOut)
+
+
+## run using HDD 3X less than the above function
+#
+#
+def runCofactors_hdd(rpcofactors, inputTar, outputTar, pathId='rp_pathway', compartment_id='MNXC3'):
+    if not os.path.exists(os.getcwd()+'/tmp'):
+        os.mkdir(os.getcwd()+'/tmp')
+    tmpInputFolder = os.getcwd()+'/tmp/'+''.join(random.choice(string.ascii_lowercase) for i in range(15))
+    tmpOutputFolder = os.getcwd()+'/tmp/'+''.join(random.choice(string.ascii_lowercase) for i in range(15))
+    os.mkdir(tmpInputFolder)
+    os.mkdir(tmpOutputFolder)
+    tar = tarfile.open(inputTar, 'r:xz')
+    tar.extractall(path=tmpInputFolder)
+    tar.close()
+    for sbml_path in glob.glob(tmpInputFolder+'/*'):
+        fileName = sbml_path.split('/')[-1].replace('.sbml', '')
+        rpsbml = rpSBML.rpSBML(fileName)
+        rpsbml.readSBML(sbml_path)
+        rpcofactors.addCofactors(rpsbml, compartment_id, pathId)
+        rpsbml.writeSBML(tmpOutputFolder)
+        rpsbml = None
+    with tarfile.open(outputTar, mode='w:xz') as ot:
+        for sbml_path in glob.glob(tmpOutputFolder+'/*'):
+            fileName = str(sbml_path.split('/')[-1].replace('.sbml', ''))
+            info = tarfile.TarInfo(fileName)
+            info.size = os.path.getsize(sbml_path)
+            ot.addfile(tarinfo=info, fileobj=open(sbml_path, 'rb'))
+    shutil.rmtree(tmpInputFolder)
+    shutil.rmtree(tmpOutputFolder)
+
+
+## REST Query
+#
+# REST interface that generates the Design.
+# Avoid returning numpy or pandas object in
+# order to keep the client lighter.
+class RestQuery(Resource):
+    def post(self):
+        inputTar = request.files['inputTar']
+        params = json.load(request.files['data'])
+        #pass the cache parameters to the rpCofactors object
+        outputTar = io.BytesIO()
+        rpcofactors = rpCofactors.rpCofactors()
+        rpcofactors.deprecatedMNXM_mnxm = rpcache.deprecatedMNXM_mnxm
+        rpcofactors.deprecatedMNXR_mnxr = rpcache.deprecatedMNXR_mnxr
+        rpcofactors.mnxm_strc = rpcache.mnxm_strc
+        rpcofactors.full_reactions = rpcache.full_reactions
+        rpcofactors.chemXref = rpcache.chemXref
+        rpcofactors.rr_reactions = rpcache.rr_reactions
+        ######## HDD #######
+        runCofactors_hdd(rpcofactors, inputTar, outputTar, params['path_id'], params['compartment_id'])
+        ######## MEM #######
+        #runCofactors_mem(rpcofactors, inputTar, outputTar, params['path_id'], params['compartment_id'])
+        ###### IMPORTANT ######
+        outputTar.seek(0)
+        #######################
+        return send_file(outputTar, as_attachment=True, attachment_filename='rpCofactors.tar', mimetype='application/x-tar')
+
+
+api.add_resource(RestApp, '/REST')
+api.add_resource(RestQuery, '/REST/Query')
+
+
+if __name__== "__main__":
+    app.run(host="0.0.0.0", port=8996, debug=True, threaded=True)
+
