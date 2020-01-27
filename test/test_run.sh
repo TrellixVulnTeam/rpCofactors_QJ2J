3c58d52d21e91ed25c11ab86910cd6949dc774e2
mdulac <melchior.dulac@inra.fr>
2020-01-27 18:40:34 +0100

    tested

--- test/test_run.sh
+++ test/test_run.sh
@@ -3,6 +3,6 @@
 rm -f test_output.tar
 docker run -d -p 5000:5000 --name test_rpCofactors brsynth/rpcofactors
 sleep 10
-python tool_rpCofactors.py -inputTar test_input.tar -outputTar test_output.tar -pathway_id rp_pathway -compartment_id MNXC3 -server_url http://0.0.0.0:5000/REST
+python tool_rpCofactors.py -inputTar test_input.tar -outputTar test_output.tar -pathway_id rp_pathway -compartment_id MNXC3 -server_url http://0.0.0.0:8888/REST
 docker kill test_rpCofactors
 docker rm test_rpCofactors
--- test/tool_rpCofactors.py
+++ test/tool_rpCofactors.py
@@ -6,9 +6,23 @@ Created on September 21 2019
 @description: Galaxy script to query rpCofactors REST service
 
 """
+import json
 import argparse
+import requests
 
-import rpToolServe
+def rpCofactorsUpload(inputTar,
+        pathway_id,
+        compartment_id,
+        server_url,
+        outputTar):
+    # Post request
+    data = {'pathway_id': pathway_id, 'compartment_id': compartment_id}
+    files = {'inputTar': open(inputTar, 'rb'),
+             'data': ('data.json', json.dumps(data))}
+    r = requests.post(server_url+'/Query', files=files)
+    r.raise_for_status()
+    with open(outputTar, 'wb') as ot:
+        ot.write(r.content)
 
 ##
 #
@@ -19,23 +33,10 @@ if __name__ == "__main__":
     parser.add_argument('-pathway_id', type=str)
     parser.add_argument('-compartment_id', type=str)
     parser.add_argument('-outputTar', type=str)
-    parser.add_argument('-sbml', type=str)
+    parser.add_argument('-server_url', type=str)
     params = parser.parse_args()
-    if params.sbml=='None' or params.sbml==None, or params.sbml=='':
-        if params.inputTar=='None' or params.inputTar==None or params.inputTar=='':
-            logging.error('Cannot have no SBML and no TAR input')
-            exit(0)
-        rpToolServe.main(params.inputTar,
-                         params.outputTar,
-                         params.pathway_id,
-                         params.compartment_id)
-    else:
-        #make the tar.xz 
-        with tempfile.TemporaryDirectory() as tmpOutputFolder:
-            inputTar = tmpOutputFolder+'/tmp_input.tar.xz'
-            with tarfile.open(inputTar, mode='w:xz') as tf:
-                tf.addfile(params.sbml)
-            rpToolServe.main(inputTar,
-                             params.outputTar,
-                             params.pathway_id,
-                             params.compartment_id)
+    rpCofactorsUpload(params.inputTar,
+                      params.pathway_id,
+                      params.compartment_id,
+                      params.server_url,
+                      params.outputTar) 
