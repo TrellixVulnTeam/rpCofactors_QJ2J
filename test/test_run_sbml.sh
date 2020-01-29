31d89f3700a05f7e9dc09eec2bcc2e12564702d1
mdulac <melchior.dulac@inra.fr>
2020-01-29 16:19:23 +0100

    still work

--- test/test_run_sbml.sh
+++ test/test_run_sbml.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run -v ${PWD}/inside_run.sh:/home/inside_run.sh -v ${PWD}/tool_rpCofactors.py:/home/tool_rpCofactors.py -v ${PWD}/test_input.tar:/home/test_input.tar -v ${PWD}/results/:/home/results/ --rm brsynth/rpcofactors /bin/sh /home/inside_run.sh
+docker run -v ${PWD}/inside_run_sbml.sh:/home/inside_run_sbml.sh -v ${PWD}/tool_rpCofactors.py:/home/tool_rpCofactors.py -v ${PWD}/test_rpsbml.xml:/home/test_rpsbml.xml -v ${PWD}/results/:/home/results/ --rm brsynth/rpcofactors /bin/sh /home/inside_run_sbml.sh
 
 cp results/test_output.tar .
